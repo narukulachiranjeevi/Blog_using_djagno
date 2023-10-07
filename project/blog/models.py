8a974b0c615dce9d853770679ae38cdd0e68d36a
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-07 14:13:03 +0530

    post display and indetail post view

--- project/blog/models.py
+++ project/blog/models.py
@@ -37,5 +37,5 @@ class Post(models.Model):
     published= PublishedManager()
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year,self.publish.month,self.publish.day, self.slug])
+        return reverse('blog:post_detail', args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
     
--- project/project/urls.py
+++ project/project/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin,include
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls',namespace='blog'))
+    path('blog/',include('blog.urls',namespace='blog')),
 ]
