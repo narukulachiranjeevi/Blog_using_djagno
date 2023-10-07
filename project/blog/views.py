5653edbd117f289ecb8e8e135bce88ace1a528ac
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-07 19:10:18 +0530

    Added comment model and commentform for blogs

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm   
 
 
 def post_list(request):
@@ -21,13 +21,27 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment=None
+    if request.method=='POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment=comment_form.save(commit=False)
+            new_comment.post=post
+            new_comment.save()
+    else:
+        comment_form=CommentForm()
+    return render(request,'blog/post/detail.html',{'post': post,'comments': comments,'new_comment': new_comment,'comment_form': comment_form})
+
+
 
 class PostListView(ListView):
     queryset=Post.published.all()
     context_object_name='posts'
     paginate_by=3
     template_name= 'blog/post/list.html'
+
+
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status='published')
@@ -40,3 +54,4 @@ def post_share(request,post_id):
     else:
         form = EmailPostForm()
     return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
+ 
